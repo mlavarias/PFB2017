e0b1a811bef4d2e84661b44e8bf7ba7028f703da
admin <admin@pfb03.cshl.edu>
2017-10-17 11:46:19 -0400

    added list 2

--- list.py
+++ list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 import sys
-name = sys.argv[1]
-color = sys.argv[2] 
-hobby = sys.argv[3] 
-animal = sys.argv[4] 
+name = sys.argv[1] #name is arg 1
+color = sys.argv[2] #color is arg 2
+hobby = sys.argv[3] #hobby is arg 3
+animal = sys.argv[4] #animal is arg 4
 
-print (name, color, hobby, animal)
+print (name, color, hobby, animal) #prints name color hobby and animal when entered into command line 
---
+++ list2.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+import sys
+print("My name is", sys.argv[1])
+print("My favorite color is", sys.argv[2])
+print("My hobby is", sys.argv[3])
+print("My favorite animal is", sys.argv[4])
