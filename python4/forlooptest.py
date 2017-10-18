97465f77f64e5c80a5e1e919f44c8db173caf2aa
admin <admin@pfb03.cshl.edu>
2017-10-18 16:34:01 -0400

    added forlooptest

---
+++ python4/forlooptest.py
@@ -0,0 +1,12 @@
+looplist =  [101,2,15,22,95,33,2,27,72,15,52]
+totalodd = 0
+totaleven = 0
+for itloop in looplist:
+ if itloop%2 == 0:
+  totaleven+=itloop
+ else:
+  totalodd+=itloop
+print(totaleven)
+print(totalodd)
+
+  
